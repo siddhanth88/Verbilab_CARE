5581cb5409ca8bd9599d7d4591fdb32b4bb6912a
SIDDHANTH REMMA <140525821+siddhanth88@users.noreply.github.com>
2026-05-12 16:14:15 +0530

    Update LoginPage.jsx

--- care-dashboard/src/pages/LoginPage.jsx
+++ care-dashboard/src/pages/LoginPage.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 
-const API =
-  import.meta.env.VITE_API_URL ||
-  "https://verbilabcare-production.up.railway.app";
+console.log("API URL =", API);
 
 export default function LoginPage({ onLogin }) {
   const [email, setEmail] = useState("admin@care.ai");
